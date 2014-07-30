e9a071851348a559aabacef2e8a2d98b5adbb946
Jeremy Miranda <Jeremy.D.Miranda@gmail.com>
2014-07-29 22:25:55 -0400

    reverted schemeexprdriver.java

--- SchemeExprDriver.java
+++ SchemeExprDriver.java
@@ -7,7 +7,7 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.Token;
 
-/*
+
 public class SchemeExprDriver {
     public static void main(String[] args) throws Exception {
         String inputFile = null;
@@ -25,44 +25,6 @@ public class SchemeExprDriver {
         eval.visit(tree);
     }
 }
-*/
 
 
-public class SchemeExprDriver {
-
-
-    public static void error(Token t, String msg) {
-        System.err.printf("line %d:%d %s\n", t.getLine(), t.getCharPositionInLine(),
-                msg);
-    }
-
-    public void process(String[] args) throws Exception {
-        String inputFile = null;
-        if ( args.length>0 ) inputFile = args[0];
-        InputStream is = System.in;
-        if ( inputFile!=null ) {
-            is = new FileInputStream(inputFile);
-        }
-        ANTLRInputStream input = new ANTLRInputStream(is);
-        SchemeExprLexer lexer = new SchemeExprLexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        SchemeExprParser parser = new SchemeExprParser(tokens);
-        parser.setBuildParseTree(true);
-        ParseTree tree = parser.prog();
-
-
-        ParseTreeWalker walker = new ParseTreeWalker();
-        DefPhase def = new DefPhase();
-        walker.walk(def, tree);
-
-        RefPhase ref = new RefPhase(def.globals, def.scopes);
-        walker.walk(ref, tree);
-
-        SchemeEvalVisitor eval = new SchemeEvalVisitor();
-        eval.visit(tree);
-    }
-
-    public static void main(String[] args) throws Exception {
-        new SchemeExprDriver().process(args);
-    }
 }
